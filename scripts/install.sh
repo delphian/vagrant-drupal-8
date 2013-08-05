03f53e96c2203263aad76ed586855bbb754b20d9
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-08-05 09:55:48 -0700

    Fixing install directory name.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ../vagrant/scripts
-./install.sh 33.33.33.69 local.drupal8.org /var/www/local.drupal.org 512 ../scripts/install-drupal.sh
+./install.sh 33.33.33.69 local.drupal8.org /var/www/local.drupal8.org 512 ../scripts/install-drupal.sh
